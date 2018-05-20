baa6457ed4a62528ddb8a9b08f542d34335f3bf2
Escurra96 <fernandoliseras@gmail.com>
2018-05-20 17:03:53 -0400

    agregado metodo que agrega paginas a marco

--- src/memoria/LRU.java
+++ src/memoria/LRU.java
@@ -1,13 +1,40 @@
 package memoria;
 
+import java.util.List;
+
 /**
  *
  * @author Escurra
  */
 public class LRU {
-    public LRU(Pagina[] memoriaRam, Pagina memoriaVirtual){
+    Pagina []_memoriaRam;
+    Pagina _nueva;
+    List <Pagina> _memoriaVirtual;
+    public LRU(Pagina[] memoriaRam,Pagina nueva ,List<Pagina> memoriaVirtual){
+        _memoriaRam = memoriaRam;
+        _nueva = nueva;
+        _memoriaVirtual = memoriaVirtual;
+    }
+    public void algoritmo(){
+        if(hayMarco()){
+          for(int a = 0;a<_memoriaRam.length;a++){
+            if(_memoriaRam[a] == null){
+                _memoriaRam[a] = _nueva;
+            }
+        }
+        }
         
     }
+    public boolean hayMarco(){
+        boolean hay_espacio = false;
+        for (Pagina _memoriaRam1 : _memoriaRam) {
+            if (_memoriaRam1 == null) {
+                hay_espacio = true;
+            }
+        }
+       return hay_espacio;
+    }
+    
     
     
 }
